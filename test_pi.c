d030143fff96b463d9d5313dfdaf75b9618a2dc3
cflinto <clement.flint@etu.unistra.fr>
2019-01-05 19:34:38 +0000

    ajout test_pi.c

---
+++ test_pi.c
@@ -0,0 +1,22 @@
+#include <complex.h>
+#include <stdio.h>
+#include <mpc.h>
+
+int main()
+{
+    float pi=0.f, k;
+
+    #pragma MPC precision(255) rounding(MPC_RNDZZ)
+    {
+        
+        for(k=0;k<100;k=k+1)
+        {
+            pi=pi+pow(-3, -k)/(2*k+1);
+        }
+        pi = pi*sqrt(12);
+    }
+
+    printf("d = %d\n", i);
+
+    return 0;
+}
