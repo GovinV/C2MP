094b1f42fd983a8a60657757861a4927150f0bb4
lioneljung <lionel.jung@etu.unistra.fr>
2019-01-05 23:10:23 +0100

    final test good

--- test_functions.c
+++ test_functions.c
@@ -25,15 +25,15 @@ int main()
 
     // check arrondis !
     printf("### Here are the results! We print 8 decimals ###");
-    printf("pow(2, 3) %6f 8.00000000\n", a);
-    printf("sqrt(2) %6f 1.41421356\n", b);
-    printf("exp(2) %6f 7.38905609\n", d);
-    printf("log(2) %6f 0.69314718\n", e);
-    printf("log10(100) %6f 2.00000000\n", f);
-    printf("cos(2) %6f -0.41614683\n", g);
-    printf("sin(2) %6f 0.90929742\n", h);
-    printf("cosh(2) %6f 3.76219569\n", i);
-    printf("sinh(2) %6f 3.62686040\n", j);
+    printf("pow(2, 3) %6f 8.000000\n", a);
+    printf("sqrt(2) %6f 1.414214\n", b);
+    printf("exp(2) %6f 7.389056\n", d);
+    printf("log(2) %6f 0.693147\n", e);
+    printf("log10(100) %6f 2.000000\n", f);
+    printf("cos(2) %6f -0.416147\n", g);
+    printf("sin(2) %6f 0.909297\n", h);
+    printf("cosh(2) %6f 3.762196\n", i);
+    printf("sinh(2) %6f 3.626860\n", j);
 
     return 0;
 
