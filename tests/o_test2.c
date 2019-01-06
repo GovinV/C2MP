e254f83b4a8724753d880c851a7157a013baf9ae
Govin <govin.vet@gmail.com>
2019-01-06 02:58:32 +0100

    old test not functionnal

---
+++ tests/o_test2.c
@@ -0,0 +1,24 @@
+#pragma MPC precision(128)
+{
+
+    z = g + j;
+    a = x + y;
+    b = y + x;
+    while(a<b)
+    {
+    	s=r+m;
+    	while(s>r)
+    	{
+    		w = w-1;
+
+
+    	}
+    	t=r+m;
+    }
+    e = x + y;
+    
+    x = y;
+    c = a + i;
+    d = i + a;
+
+}
